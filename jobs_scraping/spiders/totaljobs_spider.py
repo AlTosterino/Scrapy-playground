825f6c12425cae1908b6d6a410071581d74a0aba
AlTosterino <altosterino@gmail.com>
2020-04-08 10:16:41 +0200

    Add spider for totaljobs

---
+++ jobs_scraping/spiders/totaljobs_spider.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import w3lib.html
+from scrapy.exceptions import CloseSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from jobs_scraping import JobItem
+
+
+class TotaljobsSpider(CrawlSpider):
+    """Spider for extracting Python job offers from Glassdoor"""
+
+    name = "totaljobs_spider"
+    file_name = "Totaljobs"
+    initial_country = "London"
+
+    start_urls = ["https://www.totaljobs.com/jobs/python"]
+    base_url = "https://www.totaljobs.com/"
+
+    rules = (
+        Rule(
+            LinkExtractor(allow=(), restrict_css="a.btn.btn-default.next"),
+            callback="parse_item",
+            follow=True,
+        ),
+    )
+
+    custom_settings = {
+        "ITEM_PIPELINES": {"jobs_scraping.pipelines.CSVExportPipeline": 300},
+        "USER_AGENT": "",
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.current_page = kwargs.get("start_page", 1)
+        self.max_page = kwargs.get("stop_page", 1)
+        super().__init__(*args, **kwargs)
+
+    def parse_start_url(self, response):
+        return self.parse_item(response)
+
+    def parse_item(self, response):
+        """Method for scraping jobs.
+
+        @url https://www.totaljobs.com/jobs/python
+
+        @returns items 1
+
+        @scrapes position company location url country
+        """
+        if self.current_page > self.max_page:
+            raise CloseSpider("Spider has reached maximum number of pages")
+        positions = response.css("div.job-title>a>h2::text").getall()
+        locations = response.xpath(
+            "/html/body/div/div/div/div/div/div/div/div/div/div/div/div/ul/li/span[span or a]"
+        ).getall()
+        locations = [w3lib.html.remove_tags(content).strip() for content in locations]
+        companies = response.css("li.company>h3>a::text").getall()
+        urls = response.css("div.job-title>a::attr(href)").getall()
+
+        for job in zip(positions, companies, locations, urls):
+            item = JobItem()
+            position, company, location, url = job
+            item["position"] = position
+            item["company"] = company
+            item["location"] = location
+            item["url"] = url
+            item["country"] = self.initial_country
+            yield item
+        self.current_page += 1
---
+++ jobs_scraping/test/test_totaljobs_spider.py
@@ -0,0 +1,69 @@
+import os
+import unittest
+from mock import Mock
+
+from scrapy.exceptions import CloseSpider
+from scrapy.http import Response
+from jobs_scraping.spiders.totaljobs_spider import TotaljobsSpider
+
+
+class TotaljobsSpiderTests(unittest.TestCase):
+    def setUp(self):
+        self.spider = TotaljobsSpider()
+
+    def test_start_page_is_1(self):
+        self.assertEqual(self.spider.current_page, 1)
+
+    def test_start_page_can_be_initialized(self):
+        spider = TotaljobsSpider(start_page=2)
+        self.assertEqual(spider.current_page, 2)
+
+    def test_stop_page_is_1(self):
+        self.assertEqual(self.spider.max_page, 1)
+
+    def test_stop_page_can_be_initialized(self):
+        spider = TotaljobsSpider(stop_page=2)
+        self.assertEqual(spider.max_page, 2)
+
+    def test_base_url_is_substring_for_start_urls(self):
+        for link in self.spider.start_urls:
+            with self.subTest(i=link):
+                self.assertTrue(self.spider.base_url in link)
+                self.assertTrue(link.startswith(self.spider.base_url))
+
+    def test_file_name_for_spider_is_not_empty(self):
+        self.assertIsNotNone(self.spider.file_name)
+
+    def test_closing_spider_when_max_page_reached(self):
+        self.spider.current_page = 2
+        self.spider.max_page = 1
+        response_mock = Mock(spec=Response)
+        with self.assertRaises(CloseSpider):
+            [*self.spider.parse_item(response_mock)]
+        response_mock.css().getall.assert_not_called()
+
+    def test_parse_start_url_returns_parse_page(self):
+        response_mock = Mock(spec=Response)
+        response_mock.css().getall.return_value = ("Test Value",)
+        response_mock.xpath().getall.return_value = ("Test Value",)
+        data = [*self.spider.parse_start_url(response_mock)]
+        self.assertIsNotNone(data)
+        # I don't know if these test are good :/
+        spider_mock = Mock(spec=TotaljobsSpider)
+        spider_mock.parse_start_url.return_value = spider_mock.parse_item(response_mock)
+        spider_mock.parse_start_url(response_mock)
+        spider_mock.parse_start_url.assert_called_once_with(response_mock)
+        spider_mock.parse_item.assert_called_once_with(response_mock)
+
+    def test_spider_parse_job_yields_correct_item(self):
+        response_mock = Mock(spec=Response)
+        response_mock.css().getall.return_value = ("Test Value",)
+        response_mock.xpath().getall.return_value = (
+            '<span>\n  <a href="/jobs/in-nottingham">Nottingham</a>, <a href="/jobs/in-nottinghamshire">Nottinghamshire</a>\n</span>',
+        )
+        item = [*self.spider.parse_item(response_mock)][0]
+        self.assertEqual(item["company"], "Test Value")
+        self.assertEqual(item["location"], "Nottingham, Nottinghamshire")
+        self.assertEqual(item["position"], "Test Value")
+        self.assertEqual(item["url"], "Test Value")
+        self.assertEqual(item["country"], "London")
