2f2333fb5c24ec1912c7427dcd590d5b7d9d1231
AlTosterino <altosterino@gmail.com>
2020-04-09 09:36:20 +0200

    "Fix" totaljobs spider, still  broken, waiting for proxies

--- jobs_scraping/test/test_totaljobs_spider.py
+++ jobs_scraping/test/test_totaljobs_spider.py
@@ -48,31 +48,61 @@ class TotaljobsSpiderTests(unittest.TestCase):
         self.spider.max_page = 1
         response_mock = Mock(spec=Response)
         with self.assertRaises(CloseSpider):
-            _ = [*self.spider.parse_item(response_mock)]
+            _ = [*self.spider.parse_page(response_mock)]
         response_mock.css().getall.assert_not_called()
 
     def test_parse_start_url_returns_parse_page(self):
         response_mock = Mock(spec=Response)
-        response_mock.css().getall.return_value = ("Test Value",)
-        response_mock.xpath().getall.return_value = ("Test Value",)
+        test_links = (
+            f"{self.spider.start_urls[0]}?page1",
+            f"{self.spider.start_urls[0]}?page2",
+        )
+        response_mock.css().getall.return_value = test_links
         data = [*self.spider.parse_start_url(response_mock)]
-        self.assertIsNotNone(data)
+        for link in zip(data, test_links):
+            with self.subTest(i=link[1]):
+                self.assertEqual(link[0].url, link[1])
         # I don't know if these test are good :/
         spider_mock = Mock(spec=TotaljobsSpider)
-        spider_mock.parse_start_url.return_value = spider_mock.parse_item(response_mock)
+        spider_mock.parse_start_url.return_value = spider_mock.parse_page(response_mock)
         spider_mock.parse_start_url(response_mock)
         spider_mock.parse_start_url.assert_called_once_with(response_mock)
-        spider_mock.parse_item.assert_called_once_with(response_mock)
+        spider_mock.parse_page.assert_called_once_with(response_mock)
+
+    def test_spider_parse_page_yields_correct_links(self):
+        response_mock = Mock(spec=Response)
+        test_links = (
+            f"{self.spider.start_urls[0]}?page1",
+            f"{self.spider.start_urls[0]}?page2",
+        )
+        response_mock.css().getall.return_value = test_links
+        self.spider.parse_page(response_mock)
+        for link in zip([*self.spider.parse_page(response_mock)], test_links):
+            with self.subTest(i=link[1]):
+                self.assertEqual(link[0].url, link[1])
 
     def test_spider_parse_job_yields_correct_item(self):
         response_mock = Mock(spec=Response)
-        response_mock.css().getall.return_value = ("Test Value",)
-        response_mock.xpath().getall.return_value = (
-            '<span>\n  <a href="/jobs/in-nottingham">Nottingham</a>, <a href="/jobs/in-nottinghamshire">Nottinghamshire</a>\n</span>',
-        )
-        item = [*self.spider.parse_item(response_mock)][0]
+        response_mock.css().get.return_value = "Test Value"
+        response_mock.css().getall.return_value = ("Test Value1",)
+        response_mock.url = "http://testsite.com"
+        item = [*self.spider.parse_job(response_mock)][0]
         self.assertEqual(item["company"], "Test Value")
-        self.assertEqual(item["location"], "Nottingham, Nottinghamshire")
+        self.assertEqual(item["location"], "Test Value")
         self.assertEqual(item["position"], "Test Value")
-        self.assertEqual(item["url"], "Test Value")
-        self.assertEqual(item["country"], "London")
+        self.assertEqual(item["url"], "http://testsite.com")
+        self.assertEqual(item["country"], "England")
+
+    def test_spider_parse_job_set_correct_location_based_on_element(self):
+        response_mock = Mock(spec=Response)
+        response_mock.css().get.return_value = ""
+        response_mock.css().getall.return_value = ("Test Value1",)
+        response_mock.url = "http://testsite.com"
+        item = [*self.spider.parse_job(response_mock)][0]
+        self.assertEqual(item["location"], "Test Value1")
+        response_mock = Mock(spec=Response)
+        response_mock.css().get.return_value = ""
+        response_mock.css().getall.return_value = ""
+        response_mock.url = "http://testsite.com"
+        item = [*self.spider.parse_job(response_mock)][0]
+        self.assertEqual(item["location"], "No location")
